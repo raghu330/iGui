48d07861c6163db5d8354716cca0b8df9b581f05
arulalant <arulalant@gmail.com>
2015-12-07 17:37:50 +0530

    update path

--- bsub_anl_fcst_00hr.bash
+++ bsub_anl_fcst_00hr.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
 #BSUB -a poe                  # set parallel operating environment
-#BSUB -J hybrid_job_name      # job name
-#BSUB -W 00:20                # wall-clock time (hrs:mins)
+#BSUB -J um2grib2_conv        # job name
+#BSUB -W 00:30                # wall-clock time (hrs:mins)
 #BSUB -n 32                   # number of tasks in job
 #BSUB -q small              	# queue
 #BSUB -e errors.%J.hybrid     # error file name in which %J is replaced by the job ID
 #BSUB -o output.%J.hybrid     # output file name in which %J is replaced by the job ID
  
-/gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/raghav/Iris/datCnvrsn/iGui/um2grb2_parallel_v3.py
+/gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/umtid/iGui/utils/um2grb2_anl_fcst_00hr.py
+
