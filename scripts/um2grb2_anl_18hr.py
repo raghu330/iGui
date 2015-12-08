65cacac534456a089f0ece21a35f599f08175ad3
arulalant <arulalant@gmail.com>
2015-12-08 18:34:45 +0530

    updated module path; get paths from configure file

--- scripts/um2grb2_anl_18hr.py
+++ scripts/um2grb2_anl_18hr.py
@@ -10,8 +10,31 @@ Date : 07.Dec.2015
 """
 
 import os, sys 
-from um2grb2 import convertAnlFiles 
+from g2utils.um2grb2 import convertAnlFiles
+
+print "Reading configure file to load the paths"
+# get the configure lines
+clines = [l.strip() for l in open('configure').readlines() \
+                if not l.startswith(('#', '/', '!', '\n', '%'))]
+# get the dictionary keys, values
+cdic = {k.strip(): v.strip() for k,v in [l.split('=') for l in clines]}
+
+# store into local variables
+inPath = cdic.get('inPath', None)  
+outPath = cdic.get('outPath', None)
+tmpPath = cdic.get('tmpPath', None)
+
+# check the variable's path 
+for name, path in [('inPath', inPath), ('outPath', outPath), ('tmpPath', tmpPath)]:
+    if path is None:
+        raise ValueError("In configure file, '%s' path is not defined !" % name)
+    if not os.path.exists(path):
+        raise ValueError("In configure file, '%s = %s' path does not exists" % (name, path))
+    print name, " = ", path
+# end of for name, path in [...]:
+
+print "Successfully loaded the above paths from configure file!"
 
 # call analysis conversion function w.r.t data assimilated during short forecast hour.
-convertAnlFiles(hr='18')
+convertAnlFiles(inPath, outPath, tmpPath, hr='18')
 
--- scripts/um2grb2_anl_fcst_00hr.py
+++ scripts/um2grb2_anl_fcst_00hr.py
@@ -10,10 +10,33 @@ Date : 07.Dec.2015
 """
 
 import os, sys 
-from um2grb2 import convertAnlFiles, convertFcstFiles
+from g2utils.um2grb2 import convertAnlFiles, convertFcstFiles
 
-# call analysis conversion function w.r.t data assimilated during short forecast hour.
-convertAnlFiles(hr='00')
-    
-# call forecast conversion function w.r.t data assimilated at 00z long forecast hour.
-convertFcstFiles(hr='00')
+print "Reading configure file to load the paths"
+# get the configure lines
+clines = [l.strip() for l in open('configure').readlines() \
+                if not l.startswith(('#', '/', '!', '\n', '%'))]
+# get the dictionary keys, values
+cdic = {k.strip(): v.strip() for k,v in [l.split('=') for l in clines]}
+
+# store into local variables
+inPath = cdic.get('inPath', None)  
+outPath = cdic.get('outPath', None)
+tmpPath = cdic.get('tmpPath', None)
+
+# check the variable's path 
+for name, path in [('inPath', inPath), ('outPath', outPath), ('tmpPath', tmpPath)]:
+    if path is None:
+        raise ValueError("In configure file, '%s' path is not defined !" % name)
+    if not os.path.exists(path):
+        raise ValueError("In configure file, '%s = %s' path does not exists" % (name, path))
+    print name, " = ", path
+# end of for name, path in [...]:
+
+print "Successfully loaded the above paths from configure file!"
+
+## call analysis conversion function w.r.t data assimilated during short forecast hour.
+convertAnlFiles(inPath, outPath, tmpPath, hr='00')
+#    
+## call forecast conversion function w.r.t data assimilated at 00z long forecast hour.
+convertFcstFiles(inPath, outPath, tmpPath, hr='00')
