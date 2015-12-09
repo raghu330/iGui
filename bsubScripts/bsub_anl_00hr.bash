2672e3b0b1c47ecffded6034c7c3a4c0cf279e48
arulalant <arulalant@gmail.com>
2015-12-09 13:27:27 +0530

    job name update

--- bsubScripts/bsub_anl_00hr.bash
+++ bsubScripts/bsub_anl_00hr.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 #BSUB -a poe                  # set parallel operating environment
-#BSUB -J um2grib2_conv        # job name
+#BSUB -J um2grb2              # job name
 #BSUB -W 00:30                # wall-clock time (hrs:mins)
 #BSUB -n 32                   # number of tasks in job
 #BSUB -q small              	# queue
 #BSUB -e errors.%J.hybrid     # error file name in which %J is replaced by the job ID
 #BSUB -o output.%J.hybrid     # output file name in which %J is replaced by the job ID
  
-/gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/umtid/iGui/scripts/um2grb2_anl_fcst_00hr.py
+/gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/umtid/iGui/scripts/um2grb2_anl_00hr.py
 
