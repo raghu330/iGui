5e0e3ee02b7f3bedbb1e46370db426d6fc8f401d
arulalant <arulalant@gmail.com>
2015-12-09 13:07:20 +0530

    added date; using loadconfigure;

---
+++ scripts/loadconfigure.py
@@ -0,0 +1,38 @@
+"""
+This is script used to load all the parameters from configure text file 
+and cross check either all the paths are valid or not.
+
+Written by : Arulalan.T
+Date : 07.Dec.2015
+"""
+
+import os, sys 
+# get this script abspath
+scriptPath = os.path.dirname(os.path.abspath(__file__))
+
+print "Reading configure file to load the paths"
+# get the configure lines
+clines = [l.strip() for l in open(os.path.join(scriptPath, 'configure')).readlines() \
+                if not l.startswith(('#', '/', '!', '\n', '%'))]
+# get the dictionary keys, values
+cdic = {k.strip(): v.strip() for k,v in [l.split('=') for l in clines]}
+
+# store into local variables
+inPath = cdic.get('inPath', None)  
+outPath = cdic.get('outPath', None)
+tmpPath = cdic.get('tmpPath', None)
+date = cdic.get('date', 'YYYYMMDD')
+
+# check the variable's path 
+for name, path in [('inPath', inPath), ('outPath', outPath), ('tmpPath', tmpPath)]:
+    if path is None:
+        raise ValueError("In configure file, '%s' path is not defined !" % name)
+    if not os.path.exists(path):
+        raise ValueError("In configure file, '%s = %s' path does not exists" % (name, path))
+    print name, " = ", path
+# end of for name, path in [...]:
+
+# get the current date if not specified
+if date == 'YYYYMMDD': date=time.strftime('%Y%m%d')
+print "date = ", date
+print "Successfully loaded the above params from configure file!"
--- scripts/um2grb2_anl_06hr.py
+++ scripts/um2grb2_anl_06hr.py
@@ -9,34 +9,8 @@ Written by : Arulalan.T
 Date : 07.Dec.2015
 """
 
-import os, sys 
 from g2utils.um2grb2 import convertAnlFiles
-# get this script abspath
-scriptPath = os.path.dirname(os.path.abspath(__file__))
-
-print "Reading configure file to load the paths"
-# get the configure lines
-clines = [l.strip() for l in open(os.path.join(scriptPath, 'configure')).readlines() \
-                if not l.startswith(('#', '/', '!', '\n', '%'))]
-# get the dictionary keys, values
-cdic = {k.strip(): v.strip() for k,v in [l.split('=') for l in clines]}
-
-# store into local variables
-inPath = cdic.get('inPath', None)  
-outPath = cdic.get('outPath', None)
-tmpPath = cdic.get('tmpPath', None)
-
-# check the variable's path 
-for name, path in [('inPath', inPath), ('outPath', outPath), ('tmpPath', tmpPath)]:
-    if path is None:
-        raise ValueError("In configure file, '%s' path is not defined !" % name)
-    if not os.path.exists(path):
-        raise ValueError("In configure file, '%s = %s' path does not exists" % (name, path))
-    print name, " = ", path
-# end of for name, path in [...]:
-
-print "Successfully loaded the above paths from configure file!"
+from loadconfigure import inPath, outPath, tmpPath, date
 
 # call analysis conversion function w.r.t data assimilated during short forecast hour.
-convertAnlFiles(inPath, outPath, tmpPath, date=time.strftime('%Y%m%d'), hr='06')
-
+convertAnlFiles(inPath, outPath, tmpPath, date, hr='06')
