3943603dd9938e07740e46ee23c3fbd5d2eed2c5
arulalant <arulalant@gmail.com>
2015-12-09 14:13:27 +0530

    module path update

--- scripts/um2grb2_anl_06hr.py
+++ scripts/um2grb2_anl_06hr.py
@@ -10,6 +10,7 @@ Date : 07.Dec.2015
 """
 
 from g2utils.um2grb2 import convertAnlFiles
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 from loadconfigure import inPath, outPath, tmpPath, date
 
 # call analysis conversion function w.r.t data assimilated during short forecast hour.
