2f632aa20d795012c006dce65877a3985f1c742d
arulalant <arulalant@gmail.com>
2015-12-10 17:48:21 +0530

    updated bsub no of processors

--- bsubScripts/bsub_anl_00hr.bash
+++ bsubScripts/bsub_anl_00hr.bash
@@ -3,8 +3,8 @@
 #BSUB -a poe                  # set parallel operating environment
 #BSUB -J um2grb2              # job name
 #BSUB -W 00:30                # wall-clock time (hrs:mins)
-#BSUB -n 32                   # number of tasks in job
-#BSUB -q small              	# queue
+#BSUB -n 6                    # number of tasks in job
+#BSUB -q small                # queue
 #BSUB -e errors.%J.hybrid     # error file name in which %J is replaced by the job ID
 #BSUB -o output.%J.hybrid     # output file name in which %J is replaced by the job ID
  
