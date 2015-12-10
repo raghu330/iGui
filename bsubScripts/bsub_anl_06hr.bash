0e542cc282fbf66f8a9115e92d1c92e4ec548c54
arulalant <arulalant@gmail.com>
2015-12-10 17:53:14 +0530

    added first line as module load cdo

--- bsubScripts/bsub_anl_06hr.bash
+++ bsubScripts/bsub_anl_06hr.bash
@@ -7,6 +7,6 @@
 #BSUB -q small                # queue
 #BSUB -e errors.%J.hybrid     # error file name in which %J is replaced by the job ID
 #BSUB -o output.%J.hybrid     # output file name in which %J is replaced by the job ID
- 
+module load cdo/1.6.4
 /gpfs2/home/umtid/Pythons/Python-2.7.9/bin/python /gpfs2/home/umtid/iGui/scripts/um2grb2_anl_06hr.py
 
